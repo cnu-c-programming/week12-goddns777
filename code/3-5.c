faa6d5097c87e271b6808f1b2d5d5b362e4fbe0b
KIM DAEUNG <152259531+goddns777@users.noreply.github.com>
2026-05-24 22:03:12 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, const char* argv[]) {
     const char* filename = "numbers.txt";
-
     FILE* fp = fopen(filename, "r");
+    
     if (fp == NULL) {
         return 0;
     }
 
-
     int sum = 0;
+    char line[256];
 
+    while (fgets(line, sizeof(line), fp)) {
+        int val;
+        line[strcspn(line, "\n")] = 0;
+
+        if (sscanf(line, "%d", &val) == 1) {
+            sum += val;
+        } else {
+            fprintf(stderr, "invalid input %s\n", line);
+        }
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
